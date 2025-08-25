79d992841414e05212f25c72a3434451adb85711
Bojan Vrhovnik <bojan@vrhovnik.net>
2025-08-25 14:10:06 +0200

    Add CPU load generation controller and configuration files - fixing names

---
+++ src/TestsAcaSLN/TestsAca.CpuWebAppLoad/Controllers/CpuLoadController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestsAca.CpuWebAppLoad.Controllers;
+
+[ApiController]
+[Route("loads")]
+public class CpuLoadController(ILogger<CpuLoadController> logger) : ControllerBase
+{
+    [HttpGet("cpu")]
+    public IActionResult GenerateCpuLoad()
+    {
+        var threadCount = Environment.ProcessorCount * 2; 
+        logger.LogInformation("Starting CPU load generation with {ThreadCount} thread count.", threadCount);
+        
+        for (var currentThred = 0; currentThred < threadCount; currentThred++) 
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    // Tight loop with floating-point math
+                    double result = 0;
+                    for (int j = 0; j < 1000000; j++)
+                    {
+                        result += Math.Sqrt(j) * Math.Sin(j);
+                        logger.LogInformation("Intermediate result: {Result}", result);
+                    }
+                }
+            });
+        }
+
+        // Keep the main thread alive
+        Thread.Sleep(Timeout.Infinite);
+
+        logger.LogInformation("Completed CPU load generation.");
+        return Ok();
+    }
+}
