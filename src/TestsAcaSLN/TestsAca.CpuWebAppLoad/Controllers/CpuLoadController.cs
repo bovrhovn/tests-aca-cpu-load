b465aacd12a7caeae5b2742024928a0ddbaea901
Bojan Vrhovnik <bojan@vrhovnik.net>
2025-08-25 14:55:12 +0200

    Add CPU load generation endpoints with time limit and update documentation

--- src/TestsAcaSLN/TestsAca.CpuWebAppLoad/Controllers/CpuLoadController.cs
+++ src/TestsAcaSLN/TestsAca.CpuWebAppLoad/Controllers/CpuLoadController.cs
@@ -6,7 +6,33 @@ namespace TestsAca.CpuWebAppLoad.Controllers;
 [Route("loads")]
 public class CpuLoadController(ILogger<CpuLoadController> logger) : ControllerBase
 {
-    [HttpGet("cpu")]
+    [HttpGet("cpu-with-time-limit")]
+    public IActionResult GenerateCpuLoadForSeconds([FromQuery]int seconds=30)
+    {
+        var threadCount = Environment.ProcessorCount * 2;
+        logger.LogInformation("Starting CPU load generation with {ThreadCount} thread count.", threadCount);
+        var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(seconds));
+        for (var currentThred = 0; currentThred < threadCount; currentThred++) 
+        {
+            Task.Run(() =>
+            {
+                while (!cancellationTokenSource.Token.IsCancellationRequested)
+                {
+                    // Tight loop with floating-point math
+                    double result = 0;
+                    for (var j = 0; j < 1000000; j++)
+                    {
+                        result += Math.Sqrt(j) * Math.Sin(j);
+                        logger.LogInformation("Intermediate result for time limits: {Result}", result);
+                    }
+                }
+            }, cancellationTokenSource.Token);
+        }
+        logger.LogInformation("CPU load generation will run for {Seconds} seconds.", seconds);
+        return Ok($"CPU load generation started for {seconds} seconds.");
+    }
+
+    [HttpGet("cpu-no-time-limit")]
     public IActionResult GenerateCpuLoad()
     {
         var threadCount = Environment.ProcessorCount * 2; 
